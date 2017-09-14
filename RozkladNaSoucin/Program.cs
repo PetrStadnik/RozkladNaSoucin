1742e863ca2cc19e6df7789efe0360490f97e52d
ps <peta.stadnik@hotmail.cz>
2017-09-14 12:50:44 +0200

    Přidejte soubory projektu.

---
+++ RozkladNaSoucin/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RozkladNaSoucin
+{
+    class Program
+    {
+        public List<int> list;
+
+        static void Main(string[] args)
+        {
+            
+            
+            int x = 1;
+            Program program = new Program();
+            Console.WriteLine("Vítejte v aplikaci pro prvočíselný rozklad čísel");
+
+            while (x > 0)
+            {
+
+
+                Console.WriteLine("\nZadejte číslo! (0 pro ukončení)");
+
+                while (!int.TryParse(Console.ReadLine(), out x))
+                    Console.WriteLine("Neplatné číslo, zadejte prosím znovu:");
+                Console.WriteLine("Rozkald:");
+                foreach ( int p in program.Vypocitej(x))
+                {
+                    Console.Write(" {0}", p);
+                }
+                Console.Write("\n");
+
+            }
+
+        }
+
+        public List<int> Vypocitej(int x)
+        {
+            list = new List<int>();
+            
+            for (int i = 1; i <= x; i++)
+            {
+                
+                if (x % i == 0)
+                {
+                    list.Add(i);
+                    x = x / i;
+                    i = 1;
+                }
+
+                   
+            }
+            
+
+
+            return list;
+        }
+    }
+}
