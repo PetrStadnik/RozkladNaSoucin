1a9359bbc7b8ba0790253b2d29df500e5a2e7d1b
Petr Stadnik <peta.stadnik@email.cz>
2017-12-26 20:02:27 +0100

    Přidání funkce počítání dvojic součinitelů

---
+++ RozkladNaSoucin/Dvojce.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RozkladNaSoucin
+{
+    class Dvojce
+    {
+      
+
+        public Dvojce ()
+        {
+            Console.WriteLine("Já jsem nová třída");
+            
+        }  
+
+        public List<DvojceSoucinitelu> VypoctiDvojce(int x)
+        {
+            List<DvojceSoucinitelu> dvojceList = new List<DvojceSoucinitelu>();
+            
+            int y;
+            double z = Math.Pow(x, (1.0/2.0));
+            for (int p=1; p<= z ; p++)
+            {
+                if (x % p == 0)
+                {
+                    y = x / p;
+                    dvojceList.Add(new DvojceSoucinitelu(p, y));
+                }
+            }
+        
+
+            return dvojceList;
+        }
+    }
+
+    class DvojceSoucinitelu
+    {
+        public DvojceSoucinitelu(int cislo1, int cislo2){ Cislo1 = cislo1; Cislo2 = cislo2; }
+        public int Cislo1 { get; set; }
+        public int Cislo2 { get; set; }
+    }
+}
--- RozkladNaSoucin/Program.cs
+++ RozkladNaSoucin/Program.cs
@@ -17,7 +17,7 @@ namespace RozkladNaSoucin
             int x = 1;
             Program program = new Program();
             Console.WriteLine("Vítejte v aplikaci pro prvočíselný rozklad čísel");
-
+            Dvojce dvojce = new Dvojce();
             while (x > 0)
             {
 
@@ -31,8 +31,14 @@ namespace RozkladNaSoucin
                 {
                     Console.Write(" {0}", p);
                 }
+                
                 Console.Write("\n");
-
+                Console.WriteLine("Možné dvojce součititelů: {0}", dvojce.VypoctiDvojce(x).Count());
+                foreach (var e in dvojce.VypoctiDvojce(x))
+                {
+                    Console.WriteLine(" {0} {1}", e.Cislo1, e.Cislo2);
+                }
+               
             }
 
         }
@@ -41,7 +47,7 @@ namespace RozkladNaSoucin
         {
             list = new List<int>();
             
-            for (int i = 1; i <= x; i++)
+            for (int i = 2; i <= x; i++)
             {
                 
                 if (x % i == 0)
